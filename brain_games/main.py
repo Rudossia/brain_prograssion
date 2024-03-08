ed336d6303fdd832db083560cefce77057df1c90
Bessonov Rudolf <rebunok@gmail.com>
2024-03-08 12:00:11 +0300

    fr brain

---
+++ brain_games/main.py
@@ -0,0 +1,20 @@
+import gg
+
+
+
+
+def main():
+
+    print('Welcome to the Brain Game\n')
+
+    help = 0
+    print('Press [1] - if you want to go to help menu\n [0] - if you want to skip')
+    help = int(intput())
+    
+    if help == 1:
+        print('--help:\n[1] - About game, rules\n')
+        help_menu(choice)
+        
+    
+ if __name__ == "__main__":
+    main()
